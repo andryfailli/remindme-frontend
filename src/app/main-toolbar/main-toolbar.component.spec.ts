88f2b1f72da5eb851d95eeca511b75be4f1032c8
Andrea Failli <andry.failli@gmail.com>
2018-01-19 17:49:00 +0100

    hide main toolbar user photo and menu button if user not logged in

--- src/app/main-toolbar/main-toolbar.component.spec.ts
+++ src/app/main-toolbar/main-toolbar.component.spec.ts
@@ -1,6 +1,10 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Observable } from 'rxjs/Observable';
+
+import { AuthService } from '../auth-service/auth.service';
+import { User } from '../models/user.model';
 import { MainToolbarComponent } from './main-toolbar.component';
 
 describe('MainToolbarComponent', () => {
@@ -11,7 +15,15 @@ describe('MainToolbarComponent', () => {
     async(() => {
       TestBed.configureTestingModule({
         declarations: [MainToolbarComponent],
-        schemas: [NO_ERRORS_SCHEMA]
+        schemas: [NO_ERRORS_SCHEMA],
+        providers: [
+          {
+            provide: AuthService,
+            useValue: {
+              user$: Observable.of(new User())
+            }
+          }
+        ]
       }).compileComponents();
     })
   );
