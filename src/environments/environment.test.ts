f916847f60a5a549467e2cd9873a64dc2d576757
Andrea Failli <andry.failli@gmail.com>
2018-02-01 11:28:30 +0100

    added test environment for e2e

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://remindme.apps.andreafailli.it/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAKt676qPH-JLdPjvMNEuM-6g-fn_Pt2Ow',
+    authDomain: 'glass-crossing.firebaseapp.com',
+    databaseURL: 'https://glass-crossing.firebaseio.com',
+    projectId: 'glass-crossing',
+    storageBucket: 'glass-crossing.appspot.com',
+    messagingSenderId: '806923153240'
+  }
+};
